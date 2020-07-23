976ac1b6b4f7a28e7a280aa371a86d106ed7185b
Hugo Zabini <hugo.zabini98@hotmail.com>
2020-07-23 03:27:18 -0300

    Create seeds to create fkae users

---
+++ backend/database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory('App\User', 10)->create();
+    }
+}
